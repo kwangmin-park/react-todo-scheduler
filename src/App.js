63dfce4a01d52cbff8a7aec185bd98a1c72ba8d9
huiae07 <huiae07@gmail.mail>
2021-11-07 23:56:02 +0900

    toggle, insert, remove 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -21,12 +21,35 @@ const App = () => {
             checked: false,
         },
     ]);
+
+    //id 값이 변경된다고 리렌더링을 할 필요가 없으므로 state가 아닌 ref를 사용한다.
+    const nextId = useRef(4);
+
+    //props로 넘기는 함수는 useCallback를 사용한다. todos를 사용하므로 todos가 바뀔때마다 함수 새로 만들도록 넘겨준다.
+    const onInsert = useCallback(text => {
+        const todo = {
+            id: nextId.current,
+            text,
+            checked: false,
+        };
+        setTodos(todos.concat(todo));
+        nextId.current += 1;
+    },[todos]);
+
+    const onRemove = useCallback(id => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }, [todos]);
+
+    const onToggle = useCallback(id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, checked:!todo.checked} : todo))
+    }, [todos]);
+
     return (
         <TodoTemplate>
-            <TodoInsert/>
-            <TodoList todos={todos}/>
+            <TodoInsert onInsert = {onInsert}/>
+            <TodoList todos={todos} onRemove = {onRemove} onToggle={onToggle}/>
         </TodoTemplate>
     );
-}
+};
 
 export default App;
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    //컴포넌트가 리렌더링 될때마다 함수를 새로 만들지 않고, 한번 함수를 만들어서 재사용한다.
+    //useCallback 함수 내에서 state를 사용하지 않아서 가능한 방법.
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        //submit의 페이지 새로고침 방지
+        e.preventDefault();
+    }, [onInsert, value]);
+
     return (
-        <form className = "TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
-                <MdAdd />
+                <MdAdd/>
             </button>
         </form>
     );
